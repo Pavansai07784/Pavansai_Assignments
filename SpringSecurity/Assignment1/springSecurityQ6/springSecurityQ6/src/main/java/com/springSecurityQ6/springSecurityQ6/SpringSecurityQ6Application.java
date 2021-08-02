39c76c4ef97f9bc78a87420f0fce6549e611e308
Pavansai-rishi <79378585+Pavansai-rishi@users.noreply.github.com>
2021-08-02 21:32:23 +0530

    Q6

---
+++ SpringSecurity/Assignment1/springSecurityQ6/springSecurityQ6/src/main/java/com/springSecurityQ6/springSecurityQ6/SpringSecurityQ6Application.java
@@ -0,0 +1,13 @@
+package com.springSecurityQ6.springSecurityQ6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityQ6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityQ6Application.class, args);
+	}
+
+}
---
+++ SpringSecurity/Assignment1/springSecurityQ6/springSecurityQ6/src/test/java/com/springSecurityQ6/springSecurityQ6/SpringSecurityQ6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springSecurityQ6.springSecurityQ6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityQ6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
