3c59057653ce45f39b68024b6cd671e055d36522
Pavansai-rishi <79378585+Pavansai-rishi@users.noreply.github.com>
2021-08-02 21:31:17 +0530

    Q2

---
+++ SpringSecurity/Assignment1/Custom/src/main/java/com/Q6/ApplicationWithPassword/ApplicationWithPassword.java
@@ -0,0 +1,13 @@
+	package com.Q6.ApplicationWithPassword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class  ApplicationWithPassword{
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationWithPassword.class, args);
+	}
+
+}
---
+++ SpringSecurity/Assignment1/Custom/src/test/java/com/Q6/ApplicationWithPassword/CustomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Q6.ApplicationWithPassword;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
