954283bcd12a958ea61ee07521c8b499562ce124
Pavansai-rishi <79378585+Pavansai-rishi@users.noreply.github.com>
2021-08-03 16:46:36 +0530

    Add files via upload

---
+++ springCore - 1/springcore_Q4/Config.java
@@ -0,0 +1,9 @@
+package com.springcore_Q4.springcore_Q4;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.springcore_Q4.springcore_Q4")
+public class Config {
+
+}
