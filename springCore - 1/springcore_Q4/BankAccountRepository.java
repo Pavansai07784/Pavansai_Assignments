67fa4a1a42305f5a713b554ce3153f0531cb7b8b
Pavansai-rishi <79378585+Pavansai-rishi@users.noreply.github.com>
2021-08-03 17:00:16 +0530

    Delete springCore - 1/springcore_Q4 directory

--- springCore - 1/springcore_Q4/BankAccountRepository.java
+++
@@ -1,8 +0,0 @@
-package com.springcore_Q4.springcore_Q4;
-
-public interface BankAccountRepository {
-	
-	public double getBalance(int accountId);
-
-	public double updateBalance(int accountId ,double accountBalance);	
-}
--- springCore - 1/springcore_Q4/Config.java
+++
@@ -1,9 +0,0 @@
-package com.springcore_Q4.springcore_Q4;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(basePackages = "com.springcore_Q4.springcore_Q4")
-public class Config {
-
-}
