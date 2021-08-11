fbd14a1c3846b324f63048d0c4f894caff45a98f
Pavansai-rishi <79378585+Pavansai-rishi@users.noreply.github.com>
2021-08-11 09:49:29 +0530

    Update A1Q8.ts

--- UI/Es6/Assignment1/A1Q8.ts
+++ UI/Es6/Assignment1/A1Q8.ts
@@ -1,28 +1,49 @@
 class Account {
-    constructor(id, name1, balance) {
+    constructor(id, name1, bal) {
         this.id = id;
         this.name1 = name1;
-        this.balance = balance;
+        this.bal = bal;
     }
 }
 
 class SavingsAccount extends Account {
-    constructor(id, name1, balance, intrest, credit) {
-        super(id, name1, balance);
+    constructor(id, name1, bal, intrest, credit) {
+        super(id, name1, bal);
         this.intrest = intrest;
         this.credit = credit
     }
-    bal() {
-      
-        return "hi";
+    bal1( bal,intrest,credit ) {
+        this.bal = bal;
+        this.intrest =intrest;
+        this.credit =credit;
+        return this.bal * this.intrest*this.credit;
+
+    }
+}
+
+class CurrentAccount extends Account {
+    constructor(id, name1, bal, intrest, credit) {
+        super(id, name1, bal);
+        this.intrest = intrest;
+        this.credit = credit;
+    }
+    bal2( bal,intrest,credit ) {
+        this.bal = bal;
+        this.intrest =intrest;
+        this.credit =credit;
+        return this.bal * this.intrest*this.credit*0.5;
 
     }
 }
 
 
-var acc = new SavingsAccount();
+var acc1 = new SavingsAccount();
+var acc2 = new CurrentAccount();
 
-document.write(acc.bal());
+document.write(acc1.bal1(1,2,3));
+console.log(acc1.bal1(1,2,3))
 
 
-//# sourceMappingURL=A1Q8.js.map
+
+document.write(acc2.bal2(1,2,3));
+console.log(acc2.bal2(1,2,3))
