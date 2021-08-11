0a074f2705b4c54c5f514cb1c8e26e8bdc28cf94
Pavansai-rishi <79378585+Pavansai-rishi@users.noreply.github.com>
2021-08-11 11:24:00 +0530

    Update A1Q8.js

--- UI/Es6/Assignment1/scripts/A1Q8.js
+++ UI/Es6/Assignment1/scripts/A1Q8.js
@@ -14,27 +14,50 @@ var __extends = (this && this.__extends) || (function () {
     };
 })();
 var Account = /** @class */ (function () {
-    function Account(id, name1, balance) {
+    function Account(id, name1, bal) {
         this.id = id;
         this.name1 = name1;
-        this.balance = balance;
+        this.bal = bal;
     }
     return Account;
 }());
 var SavingsAccount = /** @class */ (function (_super) {
     __extends(SavingsAccount, _super);
-    function SavingsAccount(id, name1, balance, intrest, credit) {
-        var _this = _super.call(this, id, name1, balance) || this;
+    function SavingsAccount(id, name1, bal, intrest, credit) {
+        var _this = _super.call(this, id, name1, bal) || this;
         _this.intrest = intrest;
         _this.credit = credit;
         return _this;
     }
-    SavingsAccount.prototype.bal = function () {
-        return "hi";
+    SavingsAccount.prototype.bal1 = function (bal, intrest, credit) {
+        this.bal = bal;
+        this.intrest = intrest;
+        this.credit = credit;
+        return this.bal * this.intrest * this.credit;
     };
     return SavingsAccount;
 }(Account));
-var acc = new SavingsAccount();
-document.write(acc.bal());
+var CurrentAccount = /** @class */ (function (_super) {
+    __extends(CurrentAccount, _super);
+    function CurrentAccount(id, name1, bal, intrest, credit) {
+        var _this = _super.call(this, id, name1, bal) || this;
+        _this.intrest = intrest;
+        _this.credit = credit;
+        return _this;
+    }
+    CurrentAccount.prototype.bal2 = function (bal, intrest, credit) {
+        this.bal = bal;
+        this.intrest = intrest;
+        this.credit = credit;
+        return this.bal * this.intrest * this.credit * 0.5;
+    };
+    return CurrentAccount;
+}(Account));
+var acc1 = new SavingsAccount();
+var acc2 = new CurrentAccount();
+document.write(acc1.bal1(1, 2, 3));
+console.log(acc1.bal1(1, 2, 3));
+document.write(acc2.bal2(1, 2, 3));
+console.log(acc2.bal2(1, 2, 3));
 //# sourceMappingURL=A1Q8.js.map
 //# sourceMappingURL=A1Q8.js.map
