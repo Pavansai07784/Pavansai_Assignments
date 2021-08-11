9ecbf9b95c78460e2c9e349636d10ab7484ea2ac
Pavansai-rishi <79378585+Pavansai-rishi@users.noreply.github.com>
2021-08-11 09:33:20 +0530

    Update A1Q4.ts

--- UI/Es6/Assignment1/A1Q4.ts
+++ UI/Es6/Assignment1/A1Q4.ts
@@ -1,9 +1,1 @@
-let arr = ["Tom", "Ivan", "jerry"];
-for (let i of arr) {
-    document.write("<br>");
-    document.write("[{");
-
-    var a = () => " name: " + i + " length " + i.length;
-    document.write(a() + ",");
-}
-document.write("}]");
+.
